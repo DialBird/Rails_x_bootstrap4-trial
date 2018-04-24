22ee641bc8bea1d1631fac0751d66efdadc800aa
Your Name <you@example.com>
2018-04-24 13:26:33 +0000

    Add Signin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
+  get 'sign_in' => 'sign_in#index'
   get 'second_home' => 'second_home#index'
   get 'third_home' => 'third_home#index'
 end
